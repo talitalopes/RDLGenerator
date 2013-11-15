e046b72d5c6ae524ebec7e6a9a765b88790451ec
Talita Lopes <talitalopes@gmail.com>
2013-11-15 16:12:08 -0200

    Adicionado arquivo de Constants.

---
+++ XPDLParser/src/br/ufrj/cos/prisma/util/Constants.java
@@ -0,0 +1,13 @@
+package br.ufrj.cos.prisma.util;
+
+public class Constants {
+
+	public static String XPDLFile = "input/test.xpdl.xml";	
+	public static String ACTIVITY_TAG_NAME = "Activity";
+	public static String MODEL_URL = "models/gef.uml";
+	public static String MODEL_OUTPUT_URL = "models/app.uml";
+	public static String PACKAGE_NAME = "?";
+	public static String PACKAGE_VAR_NAME = "appPack";
+	public static String TEMP_VAR_ASSIGNMENT = "$0000RDLTempVar";
+
+}
