083d77c6d898965a7bd1ea914feccfea0fa5888a
Talita Lopes <talitalopes@gmail.com>
2013-11-15 16:03:45 -0200

    - Adicionado método para encontrar nó inicial e final - Adicionado .gitignore

--- XPDLParser/src/br/ufrj/cos/prisma/Parser/Parser.java
+++ XPDLParser/src/br/ufrj/cos/prisma/Parser/Parser.java
@@ -1,12 +1,39 @@
 package br.ufrj.cos.prisma.Parser;
 
+import java.util.Iterator;
+
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
-public class Parser {
+import br.ufrj.cos.prisma.util.Util;
 
+public class Parser {
+	private static DirectedGraph<Node, DefaultEdge> g;
+	private static Node startNode;
+	private static Node endNode;
+	
 	public static void main(String[] args) {
-		DirectedGraph<Node, DefaultEdge> g = XPDLGraph.getGraph("input/test.xpdl.xml");
+		g = XPDLGraph.getGraph("input/test.xpdl.xml");
+		
+		findStartAndEndVertexs();
+		
+		Util.log("Start node: " + ((Element)startNode).getAttribute("Name"));
+		Util.log("End node: " + ((Element)endNode).getAttribute("Name"));
 	}
+	
+	private static void findStartAndEndVertexs() {
+		Iterator<Node> nodes = g.vertexSet().iterator();
+		
+		while (nodes.hasNext()) {
+			Node n = nodes.next();
+			if (g.inDegreeOf(n) == 0)
+				startNode = n;
+			if (g.outDegreeOf(n) == 0)
+				endNode = n;
+		}
+		
+	}
+	
 }
---
+++ XPDLParser/src/br/ufrj/cos/prisma/util/Util.java
@@ -0,0 +1,9 @@
+package br.ufrj.cos.prisma.util;
+
+public class Util {
+
+	public static void log(String message) {
+		System.out.println(message);
+	}
+
+}
