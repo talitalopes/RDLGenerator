cd6c694e2eba9f33da79947e1473e1f16d25b29a
Talita Lopes <talitalopes@gmail.com>
2013-12-11 07:45:20 -0200

    Inclusão de novos métodos no model ModelNode

--- XPDLParser/src/br/ufrj/cos/prisma/model/ModelNode.java
+++ XPDLParser/src/br/ufrj/cos/prisma/model/ModelNode.java
@@ -1,21 +1,27 @@
 package br.ufrj.cos.prisma.model;
 
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public class ModelNode {
 
 	Node node;
 	boolean beginLoop;
+	boolean endLoop;
+	boolean endIf;
 	boolean visited;
 	boolean insideLoop;
+	String uniqueId;
 	
 	public ModelNode() {
 	}
 	
 	public ModelNode(Node n) {
 		this.beginLoop = false;
+		this.endLoop = false;
 		this.visited = false;
 		this.node = n;
+		this.uniqueId = this.getId()+""+ this.getName();
 	}
 	
 	public Node getNode() {
@@ -34,8 +40,24 @@ public class ModelNode {
 		this.beginLoop = beginLoop;
 	}
 
+	public boolean isEndLoop() {
+		return endLoop;
+	}
+	
+	public void setEndIf(boolean endIf) {
+		this.endIf = endIf;
+	}
+
+	public boolean isEndIf() {
+		return endIf;
+	}
+	
+	public void setEndLoop(boolean endLoop) {
+		this.endLoop = endLoop;
+	}
+
 	public boolean isVisited() {
-		return visited;
+			return visited;
 	}
 	
 	public void setVisited(boolean visited) {
@@ -49,5 +71,22 @@ public class ModelNode {
 	public void setInsideLoop(boolean insideLoop) {
 		this.insideLoop = insideLoop;
 	}
+	
+	public boolean isGateway() {
+		return getName().contains("GATEWAY");
+	}
 
+	public String getName() {
+		Element cElement = (Element) this.getNode();
+		return cElement.getAttribute("Name");
+	}
+
+	public String getId() {
+		Element cElement = (Element) this.getNode();
+		return cElement.getAttribute("Id");
+	}
+
+	public String getUniqueId() {
+		return this.uniqueId;
+	}
 }
