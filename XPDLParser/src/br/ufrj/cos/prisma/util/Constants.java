30a6bff7bad5e997923b937c4857630dc69d5a66
Talita Lopes <talitalopes@gmail.com>
2014-04-12 00:47:35 -0300

    Organizando arquivos de constantes

--- XPDLParser/src/br/ufrj/cos/prisma/util/Constants.java
+++ XPDLParser/src/br/ufrj/cos/prisma/util/Constants.java
@@ -2,25 +2,38 @@ package br.ufrj.cos.prisma.util;
 
 public class Constants {
 
-	public static final String RDL_OUTPUT = "output/rdl.xml";
+	public static final CharSequence CLASS_EXTENSION_PREFIX = "ClassExtension"; //"CLASSEXTENSION";
+	
+	public static final boolean VERBOSE = false;
+
+	//------------
+	// XPDL tags |
+	//------------
+
+	public static String XPDL_ACTIVITY_TAG = "Activity";
+	public static String XPDL_TRANSITION_TAG = "Transition";
 //	public static final String XPDLFile = "input/5-commits-log.xpdl";
-	public static final String XPDLFile = "input/gef-2apps-shapes-vainolo-classes-only.xpdl";
+	public static final String XPDLFile = "input/graphiti1.xpdl";
 //	public static final String XPDLFile = "input/test.xpdl.xml";
-	public static final String ACTIVITY_TAG_NAME = "Activity";
+
+	//------------------
+	// Causal net tags |
+	//------------------
+	
+	public static String CNET_ACTIVITY_TAG = "node";
+	public static String CNET_TRANSITION_TAG = "arc";
+	public static String CNET_TRANSITION_SOURCE_TAG = "source";
+	public static String CNET_TRANSITION_TARGET_TAG = "target";
+	public static final String CNETFile = "input/causal-net.flex";
+	
+	//----------------
+	// RDL Constants |
+	//----------------
+	public static final String RDL_OUTPUT = "output/graphiti1-rdl.xml";
 	public static final String MODEL_URL = "models/gef.uml";
 	public static final String MODEL_OUTPUT_URL = "models/app.uml";
 	public static final String PACKAGE_NAME = "?";
 	public static final String PACKAGE_VAR_NAME = "appPack";
 	public static final String TEMP_VAR_ASSIGNMENT = "$0000RDLTempVar";
-
-	public static final int JOIN_GATEWAY = 0;
-	public static final int SPLIT_GATEWAY = 1;
-	public static final CharSequence CLASS_EXTENSION_PREFIX = "CLASSEXTENSION";
-//	public static final CharSequence CLASS_EXTENSION_PREFIX = "ClassExtension";
-	
-	public static final boolean VERBOSE = false;
-	
-	public static String ACTIVITY_TAG = "Activity";
-	public static String TRANSITION_TAG = "Transition";
 
 }
